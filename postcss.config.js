dff8f8c9dc0581990889aaab9c90a992e1756aad
nobita <869594800@qq.com>
2018-08-29 11:20:34 +0800

    添加 px 2 vw适配设置。

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,17 @@ module.exports = {
   	require('import-postcss'),
   	require('postcss-mixins'),
     require('precss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('postcss-aspect-ratio-mini'),
+    require('postcss-px-to-viewport')({
+    	viewportWidth: 750,      // (Number) The width of the viewport.
+        viewportHeight: 1334,    // (Number) The height of the viewport.
+        unitPrecision: 3,        // (Number) The decimal numbers to allow the REM units to grow to.
+        viewportUnit: 'vw',      // (String) Expected units.
+        selectorBlackList: [],  // (Array) The selectors to ignore and leave as px.
+        minPixelValue: 1,       // (Number) Set the minimum pixel value to replace.
+        mediaQuery: false       // (Boolean) Allow px to be converted in media queries.
+    }),
+    require('postcss-viewport-units')
   ]
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'vw-layout';
 
 import _ from './index.css';
 
