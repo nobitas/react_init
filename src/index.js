f22ce857a9f9db6f8e23b420dc31497d2f32323d
nobita <869594800@qq.com>
2018-09-10 19:08:48 +0800

    1. 引入react-router; 2. 引入vw-layout适配方案；

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// files
+import App from './view/app';
+
+// style
 import 'vw-layout';
+import _ from './style/index.css';
 
-import _ from './index.css';
-
-const Index = ()=>{
-	return (
-		<div>
-			<h3 className={`${_.h3}`}>Hello, this is a test page.</h3>
-			<img src={require('./images/12.jpg')}/>
-			<p>32131</p>
-		</div>
-	)
-}
-ReactDOM.render(<Index/>, document.getElementById('content'));
+ReactDOM.render((
+	<Router>
+		<App/>
+	</Router>
+), document.getElementById('root'));
